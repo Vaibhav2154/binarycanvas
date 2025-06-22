7ef1d71cb54f59ab40e63cfbe543e30bb738bd09
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-06-22 23:56:24 +0530

    added analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,7 +73,23 @@ export default function RootLayout({
         {/* Touch icons */}
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <link rel="manifest" href="/site.webmanifest" />
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="https://binaryanvasvercel.matomo.cloud/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '1']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src='https://cdn.matomo.cloud/binaryanvasvercel.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+          `
+        }} />
       </head>
+      
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
