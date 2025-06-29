5b4a0be63d6fe7c4d53511ece75d06b5f25eee85
Vaibhav2154 <vaibhavvaibhu2005@gmail.com>
2025-06-29 18:44:13 +0530

    fxed home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import InnovativePortfolio from '@/components/innovative-portfolio';
+import OptimizedHome from '@/components/optimized-home';
 
 export default function Home() {
-  return <InnovativePortfolio />;
+  return <OptimizedHome />;
 }
